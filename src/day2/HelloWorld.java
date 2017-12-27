edb1553f9f029bfcd205dc11bdc308aaeda87663
AlexanderH123 <34897258+AlexanderH123@users.noreply.github.com>
2017-12-27 15:01:13 -0800

    Add files via upload

---
+++ src/day2/HelloWorld.java
@@ -0,0 +1,16 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+<<<<<<< HEAD
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "Hello World");
+}
+=======
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null,"Hello World!");
+		JOptionPane.showMessageDialog(null,"hello");
+	}
+>>>>>>> origin/master
+}
